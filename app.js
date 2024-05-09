218eff8f0877ace843c6752012db94e92f3744e6
Alexander Gomez <alexgomezgomez15@gmail.com>
2024-05-09 09:29:17 -0400

    Console logs added for each validation, Part 1 completed

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ const n4 = 5;
 // arithmetic operators and a comparison.
 const isSum50 = n1 + n2 + n3 + n4 == 50;
 
-console.log(isSum50);
+console.log(`Is the sum of all four numbers 50? ${isSum50}`);
 
 // Check two: at least two odd numbers
 // Here, we use modulus to check if something is odd.
@@ -19,13 +19,18 @@ console.log(isSum50);
 // arithmetic to count the total number of odd numbers.
 const isTwoOdd = (n1 % 2) + (n2 % 2) + (n3 % 2) + (n4 % 2) >= 2;
 
+console.log(`Are there at least 2 even numbers? ${isTwoOdd} `);
+
 // Check three: no number larger than 25
 // This time, we use the OR operator to check
 // if ANY of the numbers is larger than 25.
 const isOver25 = n1 > 25 || n2 > 25 || n3 > 25 || n4 > 25;
 
+console.log(`Are any numbers over 25? ${isOver25}`);
+
 //Change the way that isOver25 calculates so that we do not need to use the NOT operator (!) in other logic comparisons. Rename the variable as appropriate.
 const isUnder25 = n1 < 25 && n2 < 25 && n3 < 25 && n4 < 25;
+console.log(`Are all numbers under 25? ${isUnder25}`);
 
 // Check four: all unique numbers
 // This is long, and there are more efficient
@@ -33,7 +38,7 @@ const isUnder25 = n1 < 25 && n2 < 25 && n3 < 25 && n4 < 25;
 // that we will review later.
 const isUnique =
   n1 != n2 && n1 != n3 && n1 != n4 && n2 != n3 && n2 != n4 && n3 != n4;
-
+console.log(`Are all numbers unique? ${isUnique}`);
 // Here, we put the results into a single variable
 // for convenience. Note how we negate isOver25 using
 // the ! operator. We could also have tested for
@@ -41,7 +46,7 @@ const isUnique =
 const isValid = isSum50 && isTwoOdd && isUnder25 && isUnique;
 
 // Finally, log the results.
-console.log(isValid);
+console.log(`Are the four numbers valid and meet the criteria? ${isValid}`);
 
 // Here's another example of how this COULD be done,
 // but it SHOULD NOT be done this way. As programmers,
